fe4e3c0d7ccad643aada3a7d70740582721fe6c4
promethwill88 <wfong.bk@gmail.com>
2017-03-29 13:16:30 -0700

    completed some excercises

--- functions.js
+++ functions.js
@@ -1,1 +1,28 @@
 // TODO: add your solutions here!
+
+//Build your own concatenation
+//1.
+function combineWords(word1, word2){
+    let result = word1 + word2;
+    return result;
+}
+var result = combineWords('dog','house');
+console.log(result);
+
+//2. Using ES6 feature .repeat()
+function repeatPhrase(phrase, n){
+	phrase = phrase + " ";
+    let result = phrase.repeat(n);
+    console.log(result);
+}
+repeatPhrase("Hello", 5);
+
+//2. Using ES5
+function repeatPhraseAlt(phrase, n){
+    for(i = 0; i <= n; i++){
+    	phrase = phrase + " ";
+        console.log(phrase);
+    }
+}
+repeatPhrase("Hello", 5);
+
