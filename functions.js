2d78d2db630033ad538a7235e4cf0bae13980421
promethwill88 <wfong.bk@gmail.com>
2017-03-29 18:54:32 -0700

    completed further excercises

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 // TODO: add your solutions here!
 
-//Build your own concatenation
-//1.
+//1. Build your own concatenation
 function combineWords(word1, word2){
     let result = word1 + word2;
     return result;
@@ -28,13 +27,13 @@ repeatPhrase("Hello", 5);
 
 //3. Build your own Power function
 function toTheNthPower(number, power){
-    var result = 1;
-    for(var i = 0; i < power; i++){
+    let result = 1;
+    for(let i = 0; i < power; i++){
         result = result * number;
-    } 
+    	} 
     return result;
 }
-console.log(toTheNthPower(2, 3));
+toTheNthPower(2, 3);
 
 //4. Area of a circle: pie * r^2
 function areaOfCircle(r){
@@ -43,7 +42,7 @@ function areaOfCircle(r){
 	let result = pie * radiusSquared;
 	return result;
 }
-console.log(areaOfCircle(6));
+areaOfCircle(6);
 
 //5. Pythagorean Theorem: a^2 + b^2 = c^2
 function pTheorem(a, b){
@@ -52,9 +51,61 @@ function pTheorem(a, b){
     let cSquared = aSquared + bSquared;
     return cSquared;
 }
-console.log(pTheorem(2, 3));
+pTheorem(2, 3);
 
 //6. Is X Evenly Divisible by Y?
+function isDivisibleByY(x, y){
+if(x%y === 0){
+	return true;
+	}
+	else{
+	return false;
+	}
+}
+isDivisibleByY(10, 2);
+
+//7. Vowel Count
+function numberOfVowels(word){
+	let counter = 0;
+	word = word.toLowerCase();
+	for(let i = 0; i < word.length; i++){
+		let letterCheck = word[i];
+		if(letterCheck === 'a' || letterCheck === 'e' || letterCheck === 'i' || letterCheck === 'o' || letterCheck === 'u' || letterCheck === 'y'){
+			counter++;
+		}
+	}
+	return counter;
+}
+numberOfVowels('aeiou');
+
+//8. Does the array contain "wdi"
+let findString = [9, 'Will Fong', false, 'wdi'];
+function findWdi(){
+	for(let i = 0; i < findString.length; i++){
+		if(findString[i] === 'wdi'){
+			return true;
+		}
+	}
+	return false;
+}
+findWdi();
+
+//9. Find the longest word in a sentence
+function longestWord (sentence){
+	let stringToArray = sentence.split(" ");
+	let counter = 0;
+	for(let i = 0; i < stringToArray.length; i++){
+		if(stringToArray[i].length > counter){
+			counter = stringToArray[i].length;
+		}
+	}
+	return counter;
+}
+longestWord('Do a barrel roll');
+
+//10. Build an ASCII Triangle
+
+
 
 
 
